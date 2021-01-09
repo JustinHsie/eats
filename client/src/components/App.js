347ea55c9e70e43009632cb5511fe07dfd169ea0
Justin Hsie <justinhsie1001@gmail.com>
2021-01-09 14:46:09 -0800

    Created homepage with bootstrapped userlist from react-admin

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import * as React from 'react';
+import { Admin, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
 import axios from 'axios';
+import UserList from './UserList';
 
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => {
-  const response = axios.get('/');
+  //const response = axios.get('/');
 
   return (
-    <div>
-      App
-      {console.log(response)}
-    </div>
-  );
+    <Admin dataProvider={dataProvider} >
+      <Resource name="users" list={UserList} />
+    </Admin>
+  ) 
 };
 
 export default App;
