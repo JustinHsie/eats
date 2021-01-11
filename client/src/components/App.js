92d0bc3350827e20cb15caa9f41904e220696621
Justin Hsie <justinhsie1001@gmail.com>
2021-01-11 00:16:18 -0800

    Revert "Created homepage with bootstrapped userlist from react-admin"
    This reverts commit 347ea55c9e70e43009632cb5511fe07dfd169ea0.
    
    Revert homepage creation react-admin

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,15 @@
-import * as React from 'react';
-import { Admin, Resource } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
+import React from 'react';
 import axios from 'axios';
-import UserList from './UserList';
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => {
-  //const response = axios.get('/');
+  const response = axios.get('/');
 
   return (
-    <Admin dataProvider={dataProvider} >
-      <Resource name="users" list={UserList} />
-    </Admin>
-  ) 
+    <div>
+      App
+      {console.log(response)}
+    </div>
+  );
 };
 
 export default App;
