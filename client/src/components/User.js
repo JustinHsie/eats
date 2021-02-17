c283f0c8016bbc8e23110794c43b86fb8e6ddae7
Justin Hsie <justinhsie1001@gmail.com>
2021-02-16 21:11:50 -0800

    Minor style changes

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -5,16 +5,16 @@ export function User(props) {
   return (
     <div className="p-m-6 p-d-flex p-jc-center">
       <div className="card'">
-        <h2 className="p-mb-6">Account Info</h2>
-        <h3>Username:</h3>
-        <p>{props.username}</p>
+        <h2 className="p-mb-4">User</h2>
+        <h1 className="p-mb-6">{props.username}</h1>
+        <h2>Options:</h2>
         <div>{props.showPasswordForm ? props.passwordForm : null}</div>
         <div>
           {!props.showPasswordForm ? (
             <Button
               type="button"
               onClick={props.onChangePasswordClick}
-              className="p-mt-5 p-mb-2 p-button-info p-button-rounded"
+              className="p-mt-2 p-mb-2 p-button-info p-button-rounded"
               label="Change Password"
             />
           ) : null}
